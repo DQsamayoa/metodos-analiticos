944b3ed523f8ed4c939267a3eb1a60c71e1b438a
Victor Samayoa <vasamayoa@gmail.com>
2019-05-17 20:34:42 -0500

    Se incorpora el script en R que da formato a los archivos feather y los convierte en tablas de csv para después cargarlos a un bucket en S3.

---
+++ r_scripts/uploadS3.R
@@ -0,0 +1,28 @@
+library(feather)
+library(tidyverse)
+library(aws.s3)
+
+datos_path <- "datos/"
+
+cubeta <- "metodos-analiticos/proyecto_final/datos"
+
+categorias_path <- list.files(datos_path) %>% 
+                   grep(pattern = ".csv", value = TRUE, invert = TRUE) %>% 
+                   str_replace_all(pattern = "_", replacement = "-")
+
+categorias_path %>% 
+  map(function(categoria) {
+    path <- paste0(datos_path, categoria)
+    
+    datos <- list.files(path, pattern = "*.feather") %>% 
+      paste0(path, "/", .) %>% 
+      map_dfr(read_feather)
+    
+    datos_file <- paste0(path, ".csv")
+    
+    datos %>% 
+      write_csv(datos_file)
+    
+    put_object(datos_file, object = paste0(categorias_path, ".csv"), bucket = cubeta,
+               multipart = TRUE, verbose = TRUE, show_progress = TRUE)
+  })
